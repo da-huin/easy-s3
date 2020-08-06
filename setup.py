bcc4dd2800bcfc51db6e10ba3b61b42a37eee87d
Park <dahuin000@gmail.com>
2020-08-07 03:16:56 +0900

    version changed

--- setup.py
+++ setup.py
@@ -1,13 +1,12 @@
 
 # Use this command for deploy.
-#   python3 setup.py sdist bdist_wheel
-#   python3 -m twine upload --skip-existing dist/*
+#   python3 setup.py sdist bdist_wheel && python3 -m twine upload --skip-existing dist/*
 
 import io
 from setuptools import find_packages, setup
 
 setup(name='easy_s3',
-      version='0.1',
+      version='1.0',
       description='This package helps you use S3 easily.',
       long_description="Please refer to the https://github.com/da-huin/easy_s3",
       long_description_content_type="text/markdown",
@@ -17,6 +16,7 @@ setup(name='easy_s3',
       author_email='dahuin000@gmail.com',
       license='MIT',
       packages=find_packages(),
+      install_requires=["boto3", "requests"],
       classifiers=[
           'Programming Language :: Python :: 3',
     ]
